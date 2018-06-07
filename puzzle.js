2e02abc427f2d1765376f6df1db6474b803a8848
Luke <lb580@kent.ac.uk>
2018-06-07 23:12:35 +0200

    Started grid work and added controls

--- puzzle.js
+++ puzzle.js
@@ -1,3 +1,16 @@
+var canvas1 = document.getElementById('canvas1');
+var context1 = canvas1.getContext("2d");
+var canvas2 = document.getElementById('canvas2');
+var context2 = canvas2.getContext("2d");
+
+// DEFAULT TEXT //
+
+context1.font = "20px Arial";
+context1.textAlign = 'center';
+context1.fillText("Haz click o arrastra una imagen aquí",canvas1.width/2, canvas1.height/2);
+
+// DRAG AND DROP UPLOAD //
+
 var dropzone = document.getElementById('canvas1'),
 uploadBtn = document.getElementById('upload');
 dropzone.ondragenter = function(evt){
@@ -24,15 +37,18 @@ dropzone.ondrop = function(evt){
 	return false;
 }
 
+// FILE SELECT UPLOAD //
+
 var upload = document.getElementById('upload');
 upload.addEventListener('change', handleFileSelect, true);
  function handleFileSelect() {
     var file = upload.files[0]; 
     displayImg(file);
 }
+
+// DISPLAY CHOSEN IMAGE //
+
 function displayImg(file){
-	var canvas1 = document.getElementById('canvas1');
-	var context = canvas1.getContext("2d");
 	var reader = new FileReader();
 	var image = new Image();
 
@@ -40,15 +56,58 @@ function displayImg(file){
 	reader.onload = function(finishedFile){
 		image.src = finishedFile.target.result;
 		image.onload = function(){		
-				context.imageSmoothingEnabled = false;
-		context.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas1.width, canvas1.height);
+		context1.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas1.width, canvas1.height);
+		context2.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas2.width, canvas2.height);
+		drawGrid();
 		};
 		image.onerror = function(){
 			alert('Invalid file type: ' + file.type);
 		};
-	}
+	}	
 
-	
+	// DEFAULT GRID (EASY) //
+
+
 }
+
+// DRAW GRID //
+
+function drawGrid(){
+	var difficulty;
+	var pieceWidth;
+	var pieceHeight;
+	p = 0;
+
+	if(difficulty="easy"){
+		pieceWidth = ;
+		pieceHeight = ;
+	}
+	if(difficulty="medium"){
+		pieceWidth = ;
+		pieceHeight = ;
+	}
+	if(difficulty="hard"){
+		pieceWidth = ;
+		pieceHeight = ;
+	}
+	for (var x = 0; x <=canvas2.width; x+=40){
+		context2.moveTo(0.5 + x + p, p);
+		context2.lineTo(0.5 + x + p, canvas2.height + p);
+}
+	for (var x = 0; x <=canvas2.height; x+=40){
+		context2.moveTo(p, 0.5 + x + p);
+		context2.lineTo(canvas2.width + p, 0.5 + x + p);
+	}
+	context2.strokeStyle = "black";
+	context2.stroke();
+}
+
+// EASY MODE //
+
+// MEDIUM MODE //
+
+// HARD MODE //
+
+
 	
 
